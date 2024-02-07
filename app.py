67aa6ba4a74d8dd09a7aa3efdbd804d5d81fb61c
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2024-02-07 16:47:33 +1300

    v0.3

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from pywebio.platform.flask import webio_view
 import webbrowser, pywebio
 from crawler import *
+import socket
 
 init_database()
 app = Flask(__name__)
@@ -14,16 +15,19 @@ def index():
     pywebio.output.put_markdown('[Back](/)')  # put_link function is inline
     form1 = pywebio.input.input_group('Username', [
         pywebio.input.input(
-            'Phone number', type=pywebio.input.NUMBER, required=True, name='username', value='64',
+            'Phone number', type=pywebio.input.NUMBER, required=True,
+            name='username', value='64',
             help_text='Input phone number carried by Warehouse in New Zealand. '
                       'DO NOT modify the country code. DO NOT contain the leading 0.'
         ),
-        pywebio.input.checkbox(options=['Update data from Warehouse mobile website.'], name='update',
+        pywebio.input.checkbox(options=['Update usage history data.'], name='update',
                                help_text='It will take time to retrieve data.'),
-        pywebio.input.checkbox(options=['Recover incomplete recordings.'], name='recover',
-                               help_text='It will clean records in the database and download again. If you interrupted'
-                                         'the last download and have incomplete data now, please check this box.'
-                               )
+        pywebio.input.checkbox(
+            options=['Recover incomplete recordings.'], name='recover',
+            help_text='It will clean records in the database and download again. If you '
+                      'interrupted the last download and have incomplete data now, '
+                      'please check this box.'
+        )
     ])
     username = str(form1['username'])
     update_data = form1['update']
@@ -33,23 +37,25 @@ def index():
         password = get_password_from_db(username)
         if not password:
             form2 = pywebio.input.input_group('Password', [
-                pywebio.input.input('Password', name='password', type='password', required=True),
-                pywebio.input.checkbox(options=['Remember password.'], name='remember_password'),
+                pywebio.input.input('Password', name='password', type='password',
+                                    required=True),
+                pywebio.input.checkbox(options=['Remember password.'],
+                                       name='remember_password'),
             ])
             password = form2['password']
             remember_password = form2['remember_password']
             if remember_password:
                 save_password(username, password)
-        pywebio.output.put_markdown('Logging in.')
         token = get_login_token(username, password)
-        pywebio.output.put_markdown('Start to retrieve data.')
         if token:
-            retrieve(username, token, recover)
+            get_edr_full_history(username, token, recover)
+            pywebio.output.put_markdown(f'[Usage history](/report?username={username})')
+            get_data_balance(username, token)
         else:
-            pywebio.output.put_text('The saved password in the database is expired (not correct).')
+            pywebio.output.put_text(
+                'The saved password in the database is expired (not correct).')
             delete_password(username)
             return
-    pywebio.output.put_markdown(f'[View report](/report?username={username})')
 
 
 @app.route('/report', methods=['GET'])
@@ -70,8 +76,25 @@ def report():
     )
 
 
-app.add_url_rule(rule='/', endpoint='index', view_func=webio_view(index), methods=['GET', 'POST', 'OPTIONS'])
+app.add_url_rule(rule='/', endpoint='index', view_func=webio_view(index),
+                 methods=['GET', 'POST', 'OPTIONS'])
+
+
+def find_available_port(start_port):
+    tries = 100  # try times
+    for i in range(tries):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(("127.0.0.1", start_port + i))
+            s.close()
+            return start_port + i
+        except OSError:
+            pass
+    raise Exception(f"Tried {tries} times, no available port from {start_port} to "
+                    f"{start_port + tries}.")
+
 
 if __name__ == '__main__':
-    webbrowser.open_new_tab('http://localhost:5000')
-    app.run()
+    port = find_available_port(5000)
+    webbrowser.open_new_tab(f'http://localhost:{port}')
+    app.run(port=port)
--- crawler.py
+++ crawler.py
@@ -10,15 +10,26 @@ import pytz
 from math import ceil
 from pywebio.output import put_text
 
-chrome113 = {
-    'Accept': 'application/json, text/plain, */*', 'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9', 'Connection': 'keep-alive', 'Content-Length': '53',
-    'Content-Type': 'application/json;charset=UTF-8', 'DNT': '1', 'Host': 'myaccount.warehousemobile.co.nz',
-    'Origin': 'https://myaccount.warehousemobile.co.nz', 'Referer': 'https://myaccount.warehousemobile.co.nz/app/',
-    'Sec-Fetch-Dest': 'empty', 'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 '
-                  'Safari/537.36',
-    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"'
+chrome121 = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Content-Length": "59",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Dnt": "1",
+    "Host": "myaccount.warehousemobile.co.nz",
+    "Origin": "https://myaccount.warehousemobile.co.nz",
+    "Referer": "https://myaccount.warehousemobile.co.nz/app/",
+    "Sec-Ch-Ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", "
+                 "\"Chromium\";v=\"121\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                  "like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
 session = Session()
 session.trust_env = False
@@ -41,12 +52,13 @@ def init_database():
 
 
 def get_login_token(username, password):
+    put_text('Logging in.')
     login = session.post(
         url='https://myaccount.warehousemobile.co.nz/scp/login',
         data=json.dumps({'msisdn': username, 'password': password}),
-        headers=chrome113
+        headers=chrome121, verify=False
     )
-    sleep(round(uniform(0.7, 1.3), 3))
+    sleep(round(uniform(0.5, 1), 2))
     return parse_response(login).get('token')
 
 
@@ -64,12 +76,13 @@ def parse_response(response):
 
 
 def get_edr(start_date, end_date, page, username, token):
-    put_text(f"Retrieving data from {start_date.strftime('%Y-%m-%d')} to {end_date.strftime('%Y-%m-%d')}, "
-             f"page {page + 1}.")
+    put_text(f"Retrieving usage history from {start_date.strftime('%Y-%m-%d')} to "
+             f"{end_date.strftime('%Y-%m-%d')}, page {page + 1}.")
     edr_payload = {
-        # It is a mistake that, if one send a text message at 00:00:00, it will not be retrieved. However, this is the
-        # behaviour of warehouse mobile website. I keep the same behavior to prevent being detected and considered as
-        # abnormal request by warehouse mobile website.
+        # It is a mistake that, if one send a text message at 00:00:00, it will not be
+        # retrieved. However, this is the behaviour of warehouse mobile website. I keep
+        # the same behavior to prevent being detected and considered as abnormal
+        # request by warehouse mobile website.
         "token": token, "from": start_date.strftime('%Y/%m/%d 00:00:01'),
         "to": end_date.strftime('%Y/%m/%d 23:59:59'),
         "msisdn": username, "utypes": ["All"], "curpage": page, "pagesize": page_size
@@ -77,9 +90,9 @@ def get_edr(start_date, end_date, page, username, token):
     edr = session.post(
         url='https://myaccount.warehousemobile.co.nz/scp/me/edr',
         data=json.dumps(edr_payload),
-        headers=chrome113
+        headers=chrome121, verify=False,
     )
-    sleep(round(uniform(0.7, 1.3), 3))
+    sleep(round(uniform(0.5, 1), 2))
     edr_response = parse_response(edr)
     n = edr_response.get('edramount')
     record = edr_response.get('edrs')
@@ -96,7 +109,8 @@ def get_password_from_db(username):
     c = sqlite3.connect(database_path)
     r = c.cursor()
     # check if the password exists
-    r.execute('select password from tokens where username = ?', (username,))
+    r.execute('select password from tokens where username = ?',
+              (username,))
     password_db = r.fetchone()
     if password_db:
         password_db = password_db[0]
@@ -108,13 +122,16 @@ def get_password_from_db(username):
 def save_password(username, password):
     c = sqlite3.connect(database_path)
     r = c.cursor()
-    r.execute('select password from tokens where username = ?', (username,))
+    r.execute('select password from tokens where username = ?',
+              (username,))
     password_db = r.fetchone()
     if password_db:
         password_db = password_db[0]
-        r.execute('delete from tokens where username = ?', (username,))
+        r.execute('delete from tokens where username = ?',
+                  (username,))
         c.commit()
-    r.execute('insert into tokens (username, password) values (?, ?)', (username, password))
+    r.execute('insert into tokens (username, password) values (?, ?)',
+              (username, password))
     c.commit()
     r.close()
     c.close()
@@ -129,7 +146,7 @@ def delete_password(username):
     c.close()
 
 
-def retrieve(username, token, recover_mode=False):
+def get_edr_full_history(username, token, recover_mode=False):
     c = sqlite3.connect(database_path)
     r = c.cursor()
     # create the user table if not exists
@@ -152,20 +169,46 @@ def retrieve(username, token, recover_mode=False):
         end_date = yesterday
         start_date = max(last_sync, end_date - timedelta(days=28))
         while end_date > last_sync:
-            n = get_edr(start_date, end_date, 0, username, token)
-            n_pages = ceil(n / page_size)
+            n_items = get_edr(start_date, end_date, 0, username, token)
+            n_pages = ceil(n_items / page_size)
             for p in range(1, n_pages):
                 get_edr(start_date, end_date, p, username, token)
             end_date = start_date - timedelta(days=1)
             start_date = max(last_sync, end_date - timedelta(days=28))
     else:
-        n = 1
-        end_date = yesterday
-        start_date = end_date - timedelta(days=28)
-        while n > 0:
-            n = get_edr(start_date, end_date, 0, username, token)
-            n_pages = ceil(n / page_size)
+        for shift in range(6):
+            end_date = yesterday - timedelta(days=28 * shift)
+            start_date = end_date - timedelta(days=28)
+            n_items = get_edr(start_date, end_date, 0, username, token)
+            n_pages = ceil(n_items / page_size)
             for p in range(1, n_pages):
                 get_edr(start_date, end_date, p, username, token)
-            end_date = start_date - timedelta(days=1)
-            start_date = end_date - timedelta(days=28)
+
+
+def get_data_balance(username, token):
+    put_text("Retrieving statistics of remaining data package.")
+    payload = {"msisdn": username, "token": token}
+    request = session.post(
+        url='https://myaccount.warehousemobile.co.nz/scp/vp/balance',
+        data=json.dumps(payload),
+        headers=chrome121, verify=False,
+    )
+    response_text = parse_response(request)
+    put_text("Warehouse mobile only shows the earliest expiry date of data packages. "
+             "Some other data may expire after that date.")
+    for i, balance in enumerate(response_text.get("balances", [])):
+        put_text(f"[Data package {i+1}]")
+        put_text(f"Total balance is {balance.get('totalBalance')} MB.")
+        bucket_expiry = balance.get('expirationDateDataPack')
+        if bucket_expiry:
+            bucket_expiry = datetime.strptime(
+                str(bucket_expiry), "%Y%m%d%H%M%S").strftime("%Y-%m-%d %H:%M:%S")
+            put_text(f"{balance.get('closesEpiryBucketBalance')} MB will expire at "
+                     f"{bucket_expiry}. Buy a package before the expiry date, these data "
+                     f"will be extended to 28 days after your purchase.")
+        rollover_expiry = balance.get('earliestExpiringRolloverDate')
+        if rollover_expiry:
+            rollover_expiry = datetime.strptime(
+                str(rollover_expiry), "%Y%m%d%H%M%S").strftime("%Y-%m-%d %H:%M:%S")
+            put_text(f"{balance.get('earliestExpiringRolloverAmount')} MB will expire at "
+                     f"{rollover_expiry}. Rollover policy is not applicable to this.")
